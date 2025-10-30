46a53b5073cd8370e580918d0fc55088d9f24544
Rifki Jaya Afandi <afandijaya170@gmail.com>
2025-10-30 11:07:32 +0700

    fix(route): fix error suspese

--- app/(routes)/success/page.tsx
+++ app/(routes)/success/page.tsx
@@ -1,12 +1,15 @@
 import Container from "@/components/layouts/container";
 import { PaymentStatus } from "./components/payment-status";
+import { Suspense } from "react";
 
 const SuccessPage = () => {
   return (
     <Container>
       <div className="px-4 py-16 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-xl">
-          <PaymentStatus />
+          <Suspense>
+            <PaymentStatus />
+          </Suspense>
         </div>
       </div>
     </Container>
